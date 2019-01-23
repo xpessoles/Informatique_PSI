fc81f965536acfbd455970928ba998a271d90e9e
Xavier Pessoles <xpessoles.ptsi@free.fr>
2019-01-23 23:01:20 +0100

    maj

---
+++ 02_Piles/TD_02_Sable_2019/programmes/OLD/Sablier.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def circle(r,x,y):
+    les_t = np.linspace(0,2*np.pi,100)
+    les_x=(x+r*np.cos(les_t))
+    les_y=(y+r*np.sin(les_t))
+    return les_x,les_y
+    
+def trace_pile(pile):
+    for i in range(len(pile)):
+        if pile[i]=="*":
+            x,y=circle(.4,0,i)
+            plt.fill(x,y,"blue")
+        if pile[i]=="":
+            x,y=circle(.41,0,i)
+            plt.fill(x,y,"white")
+                
+def trace_chute(piles):
+    plt.plot([-larg,larg,larg,-larg,-larg],[-1,-1,haut+1,haut+1,-1])
+    plt.axis("equal")
+
+    for pile in piles:
+        trace_pile(pile)
+        plt.pause(.2) # pause avec duree en secondes
+    tmp =input("Presser une touche pour stopper...")
+    plt.close()
+
+def trace_tas(tas):
+    for i in range(len(tas)):
+        for j in range(len(tas[i])):
+            if tas[i][j]=="*":
+                x,y=circle(.4,i,j)
+                plt.fill(x,y,"blue")
+            if tas[i][j]=="":
+                x,y=circle(.41,i,j)
+                plt.fill(x,y,"white")
+
+def trace_sablier(tas):
+    plt.plot([-1,larg,larg,-1,-1],[-1,-1,haut+1,haut+1,-1])
+    plt.axis("equal")
+    for t in tas:
+        trace_tas(t)
+        plt.pause(0.01) # pause avec duree en secondes
+    tmp =input("Presser une touche pour stopper...")
+    plt.close()
